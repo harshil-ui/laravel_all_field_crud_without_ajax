c3f543375a1904877e9c3e524d9df29580237eae
Harshil <har.harshil@outlook.com>
2024-03-28 00:52:44 +0530

    Registration completed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -13,9 +15,16 @@ class UserController extends Controller
 
     public function store(UserRequest $request)
     {
+        $remember = false;
         $data = $request->validated();
         $path = $request->file('avatar')->store('public/users');
-        
+        $data['avatar'] = $path;
+        if($data['remember_me'] === 'on'){
+            $remember = true;
+        }
+        $user = User::create($data);
+        Auth::login($user, $remember);
+        return redirect(route('table'))->with('message', 'User registered successfully');
     }
 
     public function login()
--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::controller(CustomerController::class)->group(function () {
-    Route::get('/', 'create')->name('home');
-    Route::post('/store', 'store')->name('insertCustomer');
-    Route::get('/list', 'list')->name('table');
-    Route::get('/edit/{customer}', 'edit')->name('editCustomer');
-    Route::post('/update/{customer}', 'update')->name('updateCustomer');
-    Route::get('/delete/{customer}', 'delete')->name('deleteCustomer');
+Route::middleware(['auth'])->group(function () {
+    Route::controller(CustomerController::class)->group(function () {
+        Route::get('/', 'create')->name('home');
+        Route::post('/store', 'store')->name('insertCustomer');
+        Route::get('/list', 'list')->name('table');
+        Route::get('/edit/{customer}', 'edit')->name('editCustomer');
+        Route::post('/update/{customer}', 'update')->name('updateCustomer');
+        Route::get('/delete/{customer}', 'delete')->name('deleteCustomer');
+    });
+
+    Route::controller(ContractCategoryController::class)->group(function () {
+        Route::get('/create-contract', 'create')->name('create-contract');
+        Route::post('/insert-contract', 'store')->name('insert-contract');
+    });
 });
 
-Route::controller(ContractCategoryController::class)->group(function () {
-    Route::get('/create-contract', 'create')->name('create-contract');
-    Route::post('/insert-contract', 'store')->name('insert-contract');
-});
-
-Route::controller(UserController::class)->group(function () {
-    Route::get('/register', 'create')->name('register-user');
-    Route::post('/post-user', 'store')->name('post-user');
-    Route::get('/login', 'login')->name('login');
+Route::middleware(['guest'])->group(function () {
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/register', 'create')->name('register-user');
+        Route::post('/post-user', 'store')->name('post-user');
+        Route::get('/login', 'login')->name('login');
+    });
 });
